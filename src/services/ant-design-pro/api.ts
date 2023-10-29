5a497f7dd17c1ae7478c7234d3ac23b975cd9371
Timur Barmakov <tbarmakov@bk.ru>
2023-10-29 19:50:37 +0600

    ningth commit

--- src/services/ant-design-pro/api.ts
+++ src/services/ant-design-pro/api.ts
@@ -22,8 +22,8 @@ export async function outLogin(options?: { [key: string]: any }) {
 
 /** 登录接口 POST /api/login/account */
 export async function login(body: API.LoginParams, options?: { [key: string]: any }) {
-  return request<API.LoginResult>('/.netlify/functions/currentUser', {
-    method: 'GET',
+  return request<API.LoginResult>('/.netlify/functions/login', {
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
